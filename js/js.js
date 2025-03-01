dbdaee9e0ec9dc09fecb691c29488b8139be9af5
leonelpy2001 <124599074+leonelpy2001@users.noreply.github.com>
2025-03-01 16:25:56 +0100

    entrada de arquivos

---
+++ js/js.js
@@ -0,0 +1,15 @@
+
+
+let btn = document.querySelector("button")
+var span_seguindo = document.querySelector("#span-seguindo")
+span_seguindo.textContent= 1 + "M"
+btn.addEventListener("click", function () {
+  
+    this.textContent = "Seguindo"
+    this.style.opacity = 0.4
+span_seguindo.textContent= 12 + "M"
+
+
+})
+
+console.log(num)
